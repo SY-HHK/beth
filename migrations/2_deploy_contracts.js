3a193e7b4c10b46496f336e15eec2570a42e0a1d
SY-HHK <louis.ricour@gmail.com>
2021-01-23 16:24:03 +0100

    migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,12 @@
-const DappToken = artifacts.require('DappToken')
-const DaiToken = artifacts.require('DaiToken')
-const TokenFarm = artifacts.require('TokenFarm')
+const BethToken = artifacts.require('BethToken')
+const Bet = artifacts.require('Bet')
 
 module.exports = async function(deployer, network, accounts) {
-  // Deploy Mock DAI Token
-  await deployer.deploy(DaiToken)
-  const daiToken = await DaiToken.deployed()
+  // Deploy BethToken
+  await deployer.deploy(BethToken)
+  const bethToken = await BethToken.deployed()
 
-  // Deploy Dapp Token
-  await deployer.deploy(DappToken)
-  const dappToken = await DappToken.deployed()
-
-  // Deploy TokenFarm
-  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
-  const tokenFarm = await TokenFarm.deployed()
-
-  // Transfer all tokens to TokenFarm (1 million)
-  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
-
-  // Transfer 100 Mock DAI tokens to investor
-  await daiToken.transfer(accounts[1], '100000000000000000000')
+  // Deploy Beth
+  await deployer.deploy(Bet, bethToken.address)
+  const bet = await Bet.deployed()
 }
